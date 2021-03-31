67adce77d6726e46ee201191ef5f81d6818613a8
XuTongle <xutongle@gmail.com>
2021-03-31 09:20:22 +0800

    update

--- database/migrations/2020_04_14_165448_create_wallet_withdrawal_accounts_table.php
+++ database/migrations/2020_04_14_165448_create_wallet_withdrawal_accounts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateWithdrawalAccountAccountsTable extends Migration
+class CreateWalletWithdrawalAccountsTable extends Migration
 {
     /**
      * Run the migrations.
